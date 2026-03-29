e476ab43ecb6516900f948f115bb770d1e41aa94
Rafael Trindade <rafatrindade.exe@gmail.com>
2026-03-29 11:20:29 -0300

    feat(scripts): adiciona script de  ingestão de dados abertos

---
+++ scripts/extract/dados_abertos/fetch_macroregiao_de_saude.py
@@ -0,0 +1,13 @@
+from scripts.extract.dados_abertos.base_dados_abertos import LANDING_DIR, baixar_e_extrair_csv
+
+CSV_DIR = LANDING_DIR / "macroregiao"
+
+def main():
+    url = "https://arquivosdadosabertos.saude.gov.br/dados/dbgeral/macroregiao_de_saude.zip"
+    landing_file = CSV_DIR / "macroregiao.csv"
+    
+    baixar_e_extrair_csv(url, landing_file)
+    print("Lembre-se de garantir que o arquivo 'macro_geolocalizacao.xls' está na pasta Landing.")
+
+if __name__ == "__main__":
+    main()
